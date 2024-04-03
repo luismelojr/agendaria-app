c1d56216f5bb6497e4d4dfc62482ddc70dbb5034
Luis Henrique Melo <95177794+luismelojr@users.noreply.github.com>
2024-04-03 20:48:13 -0300

    feat: create menu mobile

--- resources/js/modules/landpage/screens/Home/Home.vue
+++ resources/js/modules/landpage/screens/Home/Home.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
 import Logo from '@/modules/landpage/components/Logo/Logo.vue'
+import {ref} from "vue";
+
+const openMenu = ref(false)
+
 </script>
 
 <template>
@@ -16,9 +20,23 @@ import Logo from '@/modules/landpage/components/Logo/Logo.vue'
                 <Button label="Entrar como profissional" size="small" icon="pi pi-arrow-right" icon-pos="right"/>
             </Link>
         </nav>
-        <button class="flex md:hidden">
-            mobile
-        </button>
+        <Button @click="openMenu = true" class="flex md:hidden" size="small" outlined icon="pi pi-bars" aria-label="Open menu sidebar" type="button" />
+        <Sidebar v-model:visible="openMenu" position="right" :show-close-icon="false">
+            <ul class="flex flex-col w-full gap-2">
+                <li class="w-full">
+                    <Link href="/" class="text-gray-500 rounded-md hover:bg-primary-100/10 hover:text-primary-500 w-full flex transition-all items-center gap-4 py-4 px-2 group">
+                        Entrar como cliente
+                        <span class="hidden group-hover:block transition-all"><i class="pi pi-arrow-right"/></span>
+                    </Link>
+                </li>
+                <li class="w-full">
+                    <Link href="/" class="text-gray-500 rounded-md hover:bg-primary-100/10 hover:text-primary-500 w-full flex transition-all items-center gap-4 py-4 px-2 group">
+                        Entrar como profissional
+                        <span class="hidden group-hover:block transition-all"><i class="pi pi-arrow-right"/></span>
+                    </Link>
+                </li>
+            </ul>
+        </Sidebar>
     </header>
 </template>
 
