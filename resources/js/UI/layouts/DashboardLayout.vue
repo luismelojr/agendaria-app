577536d0575c8bc674ae07f349529bc729c5f6e1
Luis Henrique Melo <95177794+luismelojr@users.noreply.github.com>
2024-04-04 20:16:27 -0300

    chore: create menu user

--- resources/js/UI/layouts/DashboardLayout.vue
+++ resources/js/UI/layouts/DashboardLayout.vue
@@ -1,34 +1,10 @@
 <script setup>
-import { ref } from "vue";
 import {Link} from "@inertiajs/vue3";
 import Logo from "@/modules/landpage/components/Logo/Logo.vue";
+import NavigationMenu from "@/UI/layouts/components/NavigationMenu.vue";
+import MenuUser from "@/UI/layouts/components/MenuUser.vue";
 
-const items = ref([
-    {
-        label: 'Dashboard',
-        icon: 'pi pi-home',
-        href: route('dashboard'),
-        routeActive: ['dashboard']
-    },
-    {
-        label: 'Cadastrar Serviço',
-        icon: 'pi pi-shopping-bag',
-        href: route('dashboard'),
-        routeActive: ['home']
-    },
-    {
-        label: 'Cadastrar Horário de Atendimento',
-        icon: 'pi pi-clock',
-        href: route('dashboard'),
-        routeActive: ['home']
-    }
-]);
 
-function isActivePage(routeName) {
-    const value = routeName.map((item) => route().current(item))
-
-    return value.includes(true)
-}
 </script>
 
 <template>
@@ -38,24 +14,8 @@ function isActivePage(routeName) {
                 <Link :href="route('dashboard')">
                     <Logo class="w-6 h-6" />
                 </Link>
-                <nav class="flex-1">
-                    <ul class="flex items-center gap-3">
-                        <li v-for="item in items" :key="item.label">
-                            <Link
-                                :href="item.href"
-
-                                class="py-2 px-4 hover:bg-slate-100 rounded-md gap-2 flex items-center text-gray-500 text-sm"
-                                :class="{'bg-primary-500/10 text-primary-500': isActivePage(item.routeActive)}"
-                            >
-                                <i :class="item.icon"></i>
-                                <span>{{ item.label }}</span>
-                            </Link>
-                        </li>
-                    </ul>
-                </nav>
-                <div class="ml-auto">
-                    a
-                </div>
+                <NavigationMenu />
+                <MenuUser />
             </div>
         </div>
         <div>
---
+++ resources/js/UI/layouts/components/MenuUser.vue
@@ -0,0 +1,42 @@
+<script setup>
+import {computed, ref} from "vue";
+import {usePage} from "@inertiajs/vue3";
+
+const menu = ref()
+const page = usePage()
+const user = computed(() => {
+    return page.props.auth.user
+})
+
+const items = ref([
+    {
+        label: 'Perfil',
+        items: [
+            {
+                label: 'Perfil',
+                icon: 'pi pi-home',
+                href: route('dashboard'),
+                routeActive: ['dashboard']
+            }
+        ]
+    },
+]);
+
+function toggleMenu(event) {
+    menu.value.toggle(event)
+}
+</script>
+
+<template>
+    <div class="ml-auto">
+        <button type="button" @click="toggleMenu">
+            <Avatar image="https://github.com/luismelojr.png" v-if="user.image_url"/>
+            <Avatar :label="user.name[0]" v-else />
+        </button>
+        <Menu ref="menu" id="overlay_menu" :model="items" :popup="true" />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
